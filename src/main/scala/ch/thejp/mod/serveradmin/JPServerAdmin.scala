986f0a40b7de60fda28e2a89f1c2dd51f7a0d4d5
TheJP <jp_55555@hotmail.com>
2015-07-23 18:33:13 +0200

    Worked on functionality

--- src/main/scala/ch/thejp/mod/serveradmin/JPServerAdmin.scala
+++ src/main/scala/ch/thejp/mod/serveradmin/JPServerAdmin.scala
@@ -5,6 +5,10 @@ import cpw.mods.fml.common.Mod
 import cpw.mods.fml.common.Mod.EventHandler
 import ch.thejp.mod.serveradmin.JPServerAdmin._
 import org.apache.logging.log4j.LogManager
+import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedOutEvent
+import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent
+import net.minecraft.server.MinecraftServer
+import net.minecraft.entity.player.EntityPlayer
 
 @Mod(modid = ModId, name = ModName, version = Version,
   modLanguage = "scala", useMetadata = true)
@@ -20,4 +24,20 @@ object JPServerAdmin {
   def init(e: FMLInitializationEvent){
     log.info("Done with init")
   }
+
+  /**
+   * Stores every online player into a file.
+   */
+  def updatePlayerList = {
+    log.info("update") //TODO: remove
+    val players = MinecraftServer.getServer.worldServers flatMap
+      { _.playerEntities.toArray map { _.asInstanceOf[EntityPlayer] } }
+    log.info(players map { _.getDisplayName } mkString { ", " })
+  }
+
+  @EventHandler
+  def join(e: PlayerLoggedInEvent) = updatePlayerList
+
+  @EventHandler
+  def leave(e: PlayerLoggedOutEvent) = updatePlayerList
 }
